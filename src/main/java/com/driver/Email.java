443829a1dacd552e6e45c226eb0bac201425ddbb
Prathamesh Khadake <pratham14785@gmail.com>
2023-10-28 19:21:17 +0530

    Attempt1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -18,6 +18,37 @@ public class Email {
         return password;
     }
 
+    public boolean isContainsUppercase(String newpassword){
+        boolean ans = false;
+        for(int i = 0; i < newpassword.length(); i++){
+            if(newpassword.charAt(i) >= 65 && newpassword.charAt(i) <= 90){
+                ans = true;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isContainsLowercase(String newpassword){
+        boolean ans = false;
+        for(int i = 0; i < newpassword.length(); i++){
+            if(newpassword.charAt(i) >= 97 && newpassword.charAt(i) <= 122){
+                ans = true;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isContainsSpecialCharcase(String newpassword){
+        boolean ans = false;
+        for(int i = 0; i < newpassword.length(); i++){
+            if((newpassword.charAt(i) >= 32 && newpassword.charAt(i) <= 47) || (newpassword.charAt(i) >= 58 && newpassword.charAt(i) <= 64) || (newpassword.charAt(i) >= 91 && newpassword.charAt(i) <= 96) || (newpassword.charAt(i) >= 123 && newpassword.charAt(i) <= 126)){
+                ans = true;
+            }
+        }
+        return ans;
+    }
+
+
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
@@ -25,5 +56,18 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(oldPassword.equals(password)){
+            if(newPassword.length() >= 8){
+                if(isContainsUppercase(newPassword)){
+                    if(isContainsLowercase(newPassword)){
+                        if(isContainsSpecialCharcase(newPassword)){
+                            this.password = newPassword;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -11,12 +11,15 @@ public class Workspace extends Gmail{
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
     public Workspace(String emailId) {
+        super(emailId, Integer.MAX_VALUE);
+        this.calendar = new ArrayList<>();
         // The inboxCapacity is equal to the maximum value an integer can store.
 
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+        calendar.add(meeting);
 
     }
 
@@ -26,5 +29,15 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
+        int maxMeeting = 0;
+        LocalTime currentTime = LocalTime.of(0,0);
+
+        for(Meeting meeting : calendar){
+            if(meeting.getStartTime().compareTo(currentTime) >= 0){
+                currentTime = meeting.getEndTime();
+                maxMeeting++;
+            }
+        }
+        return maxMeeting;
     }
 }
